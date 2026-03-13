6e45f2fe6366af1142990c1cca7a48458b6bf325
Chenzy <chenzy233@qq.com>
2026-03-13 17:05:23 +0800

    updated:banner

--- src/components/ui/Banner.jsx
+++ src/components/ui/Banner.jsx
@@ -5,13 +5,13 @@ export default function Banner() {
   return (
           <div className="relative h-lvh animate-[gradient-flow_8s_ease_infinite] overflow-hidden bg-linear-to-r from-purple-500 via-pink-500 to-yellow-200 bg-size-[200%_200%]">
         <h1 className="absolute top-2/5 left-1/10 z-10 w-1 -translate-y-1/2 text-7xl font-black text-white drop-shadow-lg">
-          Talk to the Agent
+          跟AI对话
         </h1>
         <GradientButton
           size="large"
           className="absolute top-[83%] left-[10%] z-10 -translate-y-1/2"
         >
-          <Link to="/chat">Start right now</Link>
+          <Link to="/chat">马上开始</Link>
         </GradientButton>
       </div>
   )
